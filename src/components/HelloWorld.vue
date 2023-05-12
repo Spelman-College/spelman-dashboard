ebe75564ca7527922adf01462d0d2f57913be1fa
mattsmithtaiko <mattcs@google.com>
2023-05-12 14:29:55 -0700

    Add example table pulling data from DataCommons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
+import ExampleTable from './ExampleTable.vue'
 defineProps<{
   msg: string
 }>()
 </script>
 
 <template>
-    <h1>{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <h1>{{ msg }}</h1>
+  <h3>
+    You’ve successfully created a project with
+    <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+  </h3>
+  <ExampleTable></ExampleTable>
 </template>
