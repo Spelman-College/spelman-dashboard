239db3189dc12b8baf5673a9f697b4962637e8b2
Matt Smith <mattsmithtaiko>
2023-05-30 17:04:23 -0700

    Update script with creds file and interactive terminal mode for terraform confirmation

--- infra/sperraform.sh
+++ infra/sperraform.sh
@@ -4,4 +4,4 @@ if [[ -z $(docker images --quiet --filter "reference=infra") ]]; then
     docker build -t infra .
 fi
 
-docker run -v $(pwd):/infra infra "$@"
+docker run -it -v $(pwd):/infra --env-file .env_creds infra "$@"
