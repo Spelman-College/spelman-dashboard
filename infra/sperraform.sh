c147aa077e5a3f231167eb0c5912c81b61c98972
Matt Smith <mattsmithtaiko>
2023-06-07 15:58:15 -0700

    Update credentialing method

--- infra/sperraform.sh
+++ infra/sperraform.sh
@@ -4,4 +4,8 @@ if [[ -z $(docker images --quiet --filter "reference=infra") ]]; then
     docker build -t infra .
 fi
 
-docker run -it -v $(pwd):/infra --env-file .env_creds infra "$@"
+docker run \
+-it \
+-v $(pwd):/infra \
+-e "AWS_SHARED_CREDENTIALS_FILE=/infra/.aws_credentials" \
+infra terraform "$@"
