74ba38a250b1c933faf56d1d7b43c3e94fdde88c
Matt Smith <mattsmithtaiko>
2023-06-07 15:58:03 -0700

    Set up driftctl to find differences between AWS and Terraform state

---
+++ infra/spiftctl.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+if [[ -z $(docker images --quiet --filter "reference=infra") ]]; then
+    docker build -t infra .
+fi
+
+docker run \
+-it \
+--mount type=bind,source="$(pwd)",target=/infra \
+-e "AWS_PROFILE=driftctlrole" \
+-e "AWS_CONFIG_FILE=/infra/aws_config" \
+-e "AWS_SHARED_CREDENTIALS_FILE=/infra/.aws_credentials" \
+infra \
+driftctl "$@"
