fcb337572667d1241e47f123aa7395e1ec8a75b6
Matt Smith <mattsmithtaiko>
2023-06-09 15:38:35 -0700

    Add more resources and terraform cloud integration

--- infra/infra.tf
+++ infra/infra.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "spelmanaws"
+
+    workspaces {
+      name = "spelman-dashboard"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -68,6 +76,26 @@ resource "aws_iam_access_key" "terraform" {
 
 resource "aws_iam_group" "admins" {
   name = "Admins"
+}
+
+resource "aws_iam_group_policy_attachment" "admins_force_mfa" {
+  group      = aws_iam_group.admins.name
+  policy_arn = aws_iam_policy.force_mfa.arn
+}
+
+resource "aws_iam_group_policy_attachment" "admins_aws_account_management_full_access" {
+  group      = aws_iam_group.admins.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSAccountManagementFullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "admins_administrator_access" {
+  group      = aws_iam_group.admins.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "admins_billing" {
+  group      = aws_iam_group.admins.name
+  policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
 }
 
 resource "aws_iam_group" "terraform_full_permissions" {
@@ -203,4 +231,118 @@ resource "aws_iam_policy" "terraform_full_access" {
       }
     ]
   })
+}
+
+resource "aws_iam_user" "spelmanaws" {
+  name = "SpelmanAWS"
+}
+
+resource "aws_iam_user_policy_attachment" "spelmanaws_change_password" {
+  user       = aws_iam_user.spelmanaws.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+resource "aws_iam_user" "amiers" {
+  name = "amiers"
+}
+
+resource "aws_iam_user_policy_attachment" "amiers_change_password" {
+  user       = aws_iam_user.amiers.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+resource "aws_iam_user" "cernst" {
+  name = "cernst"
+}
+
+resource "aws_iam_user_policy_attachment" "cernst_change_password" {
+  user       = aws_iam_user.cernst.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+resource "aws_iam_user" "chasecummings" {
+  name = "chasecummings"
+}
+
+resource "aws_iam_user_policy_attachment" "chasecummings_change_password" {
+  user       = aws_iam_user.chasecummings.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+resource "aws_iam_user" "kirkmcallister" {
+  name = "kirkmcallister"
+}
+
+resource "aws_iam_user_policy_attachment" "kirkmcallister_change_password" {
+  user       = aws_iam_user.kirkmcallister.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+resource "aws_iam_user" "mattcs" {
+  name = "mattcs"
+}
+
+resource "aws_iam_user_policy_attachment" "mattcs_change_password" {
+  user       = aws_iam_user.mattcs.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+resource "aws_iam_user" "mattcs_cli" {
+  name = "mattcs-cli"
+
+  tags = {
+    AKIAV66MXICPDHM5UHCK = "Keys to assume roles for mattcs-cli"
+  }
+}
+
+resource "aws_iam_user" "terraform" {
+  name = "terraform"
+
+  tags = {
+    AKIAV66MXICPLDOMYTAE = "Terraform CLI access"
+  }
+}
+
+data "aws_iam_policy_document" "cli_assume_role" {
+  statement {
+    sid    = "Statement1"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::410061848734:user/terraform",
+        "arn:aws:iam::410061848734:user/mattcs-cli"
+      ]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "budget_service_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["budgets.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "budget_service_role" {
+  name               = "BudgetServiceRole"
+  description        = "Allows Budgets to create and manage AWS resources on your behalf"
+  assume_role_policy = data.aws_iam_policy_document.budget_service_role.json
+}
+
+resource "aws_iam_role" "read_only_access" {
+  name               = "ReadOnlyAccess"
+  description        = "ReadOnlyAccess used for driftctl command to determine drift between current infrastructure and Terraform state."
+  assume_role_policy = data.aws_iam_policy_document.cli_assume_role.json
+}
+
+resource "aws_iam_role" "terraform_full_access" {
+  name               = "TerraformFullAccess"
+  description        = "Full access for Terraform to create, destroy, and plan resources."
+  assume_role_policy = data.aws_iam_policy_document.cli_assume_role.json
 }
