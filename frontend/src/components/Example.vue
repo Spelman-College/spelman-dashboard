cc387d6c10b1d09f95d95dd3686e85ce1be50182
mattsmithtaiko <mattcs@google.com>
2023-06-21 16:39:23 -0700

    Make simple button

--- frontend/src/components/Example.vue
+++ frontend/src/components/Example.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import Button from 'primevue/button'
+
+function sayHello() {
+  alert('Hello!')
+}
 </script>
 
 <template>
-  <Button label="Hello world!" />
+  <Button label="Hello world!" @click="sayHello" />
 </template>
