e6d587ecc031f5b1fdd0da62a3fdf4ea99a94ad7
Christina Ernst <13266735+cernst122@users.noreply.github.com>
2023-07-17 11:48:03 -0500

    Cache DC API call
    Create a cache and update on new DC API requests.

--- frontend/src/components/DataDashboard.vue
+++ frontend/src/components/DataDashboard.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from 'vue';
 
 const tableItems = ref(Array.from({ length: 5 }));
 const tableCols = ref();
+const gender = ref();
 
 tableCols.value = [
   {
@@ -15,12 +16,21 @@ tableCols.value = [
   }
 ]
 
-const gender = ref();
+let apiCache = new ApiCache();
 
 async function getData(request : string) {
-  const res = await fetch(request);
-  const finalRes = await res.json();
-  tableItems.value = finalRes.observations;
+  if (this.apiCache.recordExists(request)) {
+    tableItems.value = this.apiCache.get(request).observations;
+    console.log("Cache accessed");
+  }
+
+  else {
+    const res = await fetch(request);
+    const finalRes = await res.json();
+    tableItems.value = finalRes.observations;
+    this.apiCache.set(request, finalRes);
+    console.log("Cache set");
+  }
 }
 
 function genderToDcid(gender: string): string {
@@ -76,4 +86,45 @@ export default {
   },
   methods: {},
 };
+
+type CacheResult = any;
+ 
+export class ApiCache {
+    private static instance: ApiCache;
+    private cache: { url: string; result: CacheResult }[] = [];
+ 
+    constructor() {
+        if (!!ApiCache.instance) {
+          return ApiCache.instance;
+        }
+        ApiCache.instance = this;
+        return this;
+    }
+ 
+    public set = (url: string, result: any): void => {
+        if (!this.recordExists(url)) {
+          this.cache.push({
+            url,
+            result
+          });
+        }
+    }
+ 
+    public get = (url: string): CacheResult | null => {
+        const cacheRecord = this.cache.find(_ => { 
+          return _.url === url;
+        });
+ 
+        
+        if (cacheRecord) {
+          return cacheRecord.result;
+        }
+ 
+        return null;
+    }
+    
+    public recordExists = (url: string): boolean => {
+        return !!this.get(url);
+    }
+}
 </script>
