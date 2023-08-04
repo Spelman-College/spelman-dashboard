35847f8efce0944fd1b265910a00de447a9c8ce5
mattsmithtaiko <mattcs@google.com>
2023-08-04 10:57:55 -0700

    Add images to splash page

--- frontend/src/views/Splash.vue
+++ frontend/src/views/Splash.vue
@@ -10,6 +10,15 @@
       through data, insights, and advocacy
     </h2>
     <button>Visit the Data Dashboard</button>
+    <div id="splash-graphics">
+      <img src="public/splash.png" alt="" id="splash-image" />
+      <img src="public/swiggly_red_FF6454.svg" alt="" id="swiggly-red" />
+      <div id="plusses">
+        <img src="public/plus_icon_1.svg" alt="" class="plus-icon" id="plus-icon-1" />
+        <img src="public/plus_icon_2.svg" alt="" class="plus-icon" id="plus-icon-2" />
+        <img src="public/plus_icon_3.svg" alt="" class="plus-icon" id="plus-icon-3" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +26,43 @@
 #splash {
   background-color: #22262B;
   padding: 2rem 3rem;
+}
+
+#splash-graphics {
+  position: absolute;
+  top: 20rem;
+  left: 70%;
+}
+
+#splash-image {
+  position: absolute;
+  width: 24rem;
+}
+
+#swiggly-red {
+  position: absolute;
+  top: 5rem;
+  left: -7rem;
+  width: 14rem;
+}
+
+#plusses {
+  position: absolute;
+  top: 1.5rem;
+  left: 1.5rem;
+}
+
+.plus-icon {
+  position: absolute;
+  width: 1.5rem;
+}
+
+#plus-icon-2 {
+  left: 19rem;
+}
+
+#plus-icon-3 {
+  top: 19rem;
 }
 
 h1 {
