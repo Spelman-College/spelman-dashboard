8d5e07d1429b2e68dc2d2b2ef37cb56aa71ad1df
Matt Smith <mattsmithtaiko>
2023-08-08 13:20:57 -0700

    Add carousel

--- frontend/src/views/Stories.vue
+++ frontend/src/views/Stories.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
-import '@/components/StoryCard.vue'
+import StoryCard from '@/components/StoryCard.vue'
+
+import { ref } from 'vue'
+
+const nextButton = ref(null)
+
+function nextPage() {
+  document.getElementById('nextButton')?.click()
+}
+function prevPage() {
+  document.getElementById('prevButton')?.click()
+}
+
+const stories = [{
+  "thing": "1",
+}, {
+  "thing": "2",
+}, {
+  "thing": "3",
+}, {
+  "thing": "4",
+}, {
+  "thing": "5",
+}, {
+  "thing": "6",
+}]
+
+const page = ref(0)
 </script>
 
 <template>
@@ -11,17 +38,25 @@ import '@/components/StoryCard.vue'
     </p>
 
     <div class="nav-buttons">
-      <button>&larr;</button> <button>&rarr;</button>
+      <button @click="prevPage">&larr;</button> <button @click="nextPage">&rarr;</button>
     </div>
     <div>
-      <StoryCard></StoryCard>
-      <StoryCard></StoryCard>
-      <StoryCard></StoryCard>
+      <Carousel :value="stories" :numVisible="3" :numScroll="3"
+        :pt="{ previousButton: { id: 'prevButton', class: 'hidden' }, nextButton: { id: 'nextButton', class: 'hidden' } }">
+        <template #item="slotProps">
+          <p>{{ slotProps.data.thing }}</p>
+          <StoryCard></StoryCard>
+        </template>
+      </Carousel>
     </div>
     <button>Read more stories</button>
   </div>
 </template>
 
-<style scoped>
-@import "/homepage.css"
+<style>
+@import "/homepage.css";
+
+.hidden {
+  display: none;
+}
 </style>
