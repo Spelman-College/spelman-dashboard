8bae04401f9f4774530b5afe6efe26d76152e4a3
mattsmithtaiko <mattcs@google.com>
2023-08-11 18:00:49 -0400

    Flesh out more to explore section

---
+++ frontend/src/components/ExploreCard.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const props = defineProps({
+  imgSrc: String,
+})
+</script>
+<template>
+  <div id="explore-card">
+    <div class="circle">
+      <img :src="imgSrc" />
+    </div>
+    <h1>
+      <slot name="header"></slot>
+    </h1>
+    <p>
+      <slot name="text"></slot>
+    </p>
+    <a>
+      <slot name="link"></slot> &rarr;
+    </a>
+  </div>
+</template>
+
+<style scoped>
+#explore-card {
+  width: 25rem;
+  border: 1px solid #5d6267;
+  border-radius: 1.25rem;
+  box-shadow: 0 4px 20px 0 #292929;
+  padding: 2rem;
+}
+
+#explore-card .circle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 7.3125rem;
+  height: 7.3125rem;
+  border-radius: 50%;
+  background: #ffffff;
+}
+
+#explore-card h1 {
+  font-size: 1.625rem;
+  letter-spacing: revert;
+}
+
+#explore-card a {
+  margin-top: auto;
+  margin-bottom: 2rem;
+}
+</style>
--- frontend/src/views/MoreToExplore.vue
+++ frontend/src/views/MoreToExplore.vue
@@ -1,5 +1,41 @@
+<script setup lang="ts">
+import ExploreCard from '@/components/ExploreCard.vue'
+
+const cards = [
+  {
+    imgSrc: "line_graph_feature_icon_2.svg",
+    header: "Data Dashboard",
+    text: "See the comprehensive and informative data dashboard, presenting data in a visually engaging and digestible format",
+    link: "Explore data dashboard"
+  },
+  {
+    imgSrc: "line_graph_feature_icon_2.svg",
+    header: "Data Dashboard",
+    text: "See the comprehensive and informative data dashboard, presenting data in a visually engaging and digestible format",
+    link: "Explore data dashboard"
+  },
+  {
+    imgSrc: "line_graph_feature_icon_2.svg",
+    header: "Data Dashboard",
+    text: "See the comprehensive and informative data dashboard, presenting data in a visually engaging and digestible format",
+    link: "Explore data dashboard"
+  }
+]
+</script>
 <template>
   <h2>More to explore</h2>
   <p>Dive deeper into insights, metrics, news, and more about<br />
     Black women in STEM</p>
+  <!--Put these in a CSS grid -->
+  <ExploreCard v-for="card in cards" v-key="card.header" :imgSrc="card.imgSrc">
+    <template #header>
+      {{ card.header }}
+    </template>
+    <template #text>
+      {{ card.text }}
+    </template>
+    <template #link>
+      {{ card.link }}
+    </template>
+  </ExploreCard>
 </template>
