26d2ff06426080d9baecec0a731a6e5b4bcacd4e
mattsmithtaiko <mattcs@google.com>
2023-08-14 17:50:56 -0400

    Move homepage into a single component and set up with router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,15 @@
 <script setup lang="ts">
-import RouterView from 'vue-router'
+import { RouterView } from 'vue-router'
 import Header from '@/views/Header.vue'
-import Splash from '@/views/Splash.vue'
-import ExploreData from '@/views/ExploreData.vue'
-import Stories from '@/views/Stories.vue'
-import Stat from '@/views/Stat.vue'
-import MoreToExplore from '@/views/MoreToExplore.vue'
 import Footer from '@/views/Footer.vue'
 </script>
 
 <template>
     <div id="container">
         <Header></Header>
-        <div class="view">
-            <Splash></Splash>
-        </div>
-        <div class="view gradient">
-            <ExploreData></ExploreData>
-        </div>
-        <div class="view">
-            <Stories></Stories>
-        </div>
-        <div class="view gradient">
-            <Stat></Stat>
-        </div>
-        <div class="view">
-            <MoreToExplore></MoreToExplore>
-        </div>
+        <RouterView />
         <Footer></Footer>
     </div>
-
-    <RouterView />
 </template>
 
 <style>
@@ -43,13 +22,5 @@ body {
 
 div#container {
     width: 90rem;
-}
-
-div.view {
-    padding: 2rem 3rem;
-}
-
-div.gradient {
-    background: linear-gradient(180deg, rgba(34, 38, 43, 0.00) 0%, #181818 100%);
 }
 </style>
