e74c805ed0e00e3427487e53d1546f73b379676c
Amier Smith <amiers@google.com>
2023-08-16 11:50:06 -0400

    Add nsf313_20 data to filtering

---
+++ frontend/src/data/nsf313_20/categories.ts
@@ -0,0 +1,18 @@
+export const Categories = {
+    'race': new Set<string>([
+        'Black'
+    ]),
+    'collegeOrUniversityLevel': new Set<string>([
+        '4year',
+        '2year',
+    ]),
+    'collegeOrGraduateSchoolEnrollment': new Set<string>([
+        'EnrolledInCollegeOrGraduateSchool', 
+        'EnrolledInPublicCollegeOrGraduateSchool',
+        'EnrolledInPrivateCollegeOrGraduateSchool'
+    ]),
+    'gender': new Set<string>([
+        'Female',
+        'Male',
+    ]),
+}
