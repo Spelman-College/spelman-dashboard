36ad911ed304d513d6c023ee5cfb1b2982d80c3e
isaac-looker <drumsound@google.com>
2023-08-17 23:42:52 +0000

    add college major to demo dashboard

--- frontend/src/components/DataDashboard.vue
+++ frontend/src/components/DataDashboard.vue
@@ -3,5 +3,23 @@
 </script>
 
 <template>
-    <Demo />
+    <div class="data-dashboard">
+	<Demo />
+    </div>
 </template>
+<style scoped>
+ /deep/ .data-dashboard {
+     color: black;
+     background-color: white;
+ }
+ /deep/ .title {
+     color: #4285F4;
+     font-family: Google Sans Mono;
+     font-size: 14px;
+     font-style: normal;
+     font-weight: 400;
+     line-height: normal;
+ }
+
+
+</style>
