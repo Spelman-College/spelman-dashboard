89653a78791eaea9fe85d4fd4db0bfa228116380
isaac-looker <drumsound@google.com>
2023-08-17 18:00:25 +0000

    adding dashboard view to router

---
+++ frontend/src/views/DataDashboard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import DataDashboard from '@/home/DataDashboard.vue'
+import '../../node_modules/primevue/resources/themes/md-light-indigo/theme.css'
+import '../../node_modules/primeflex/primeflex.css'
+
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+let dataset = route.params.dataset
+console.log(dataset)
+
+</script>
+
+<template>
+  <div class="view">
+    <router-link id="back-button" class="material-icons" :to="{ name: 'home' }">chevron_left</router-link>
+  </div>
+  <DataDashboard></DataDashboard>
+</template>
+
+<style scoped>
+
+#back-button {
+  font-size: 2.5rem;
+  color: #ffffff;
+  margin-bottom: 1rem;
+}
+</style>
