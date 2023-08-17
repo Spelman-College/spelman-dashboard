ba6a5771939543500a2465cd5a068edfc9031fd3
Christina Ernst <13266735+cernst122@users.noreply.github.com>
2023-08-17 15:18:45 -0500

    Revert router-links to @clicks.

--- frontend/src/views/Header.vue
+++ frontend/src/views/Header.vue
@@ -2,16 +2,26 @@
 import { useRouter } from 'vue-router';
 const router = useRouter();
 
+function goToAbout() {
+  router.push({ name: 'about' })
+}
+function goToContact() {
+  router.push({ name: 'contact' })
+}
+function goToData() {
+  router.push({ name: 'data' })
+}
+
 </script>
 
 <template>
   <div id="header">
     <span id="logo">Logo</span>
-    <span class="header-links" id="about"><router-link to="/about">About</router-link></span>
+    <span class="header-links" id="about" @click="goToAbout">About</span>
     <span class="header-links" id="resources">Resources</span>
-    <span class="header-links" id="contact"><router-link to="/contact">Contact</router-link></span>
-    <span class="header-links" id="data-dashboard"  v-if="$router.currentRoute.value.name === 'home'"><router-link to="/data">Data Dashboard</router-link></span>
-    <button id="data-dashboard" class="header-links" v-if="$router.currentRoute.value.name !== 'home'"><router-link to="/data">Data Dashboard</router-link></button>
+    <span class="header-links" id="contact" @click="goToContact">Contact</span>
+    <span class="header-links" id="data-dashboard" @click="goToData" v-if="$router.currentRoute.value.name === 'home'">Data Dashboard</span>
+    <button id="data-dashboard" class="header-links" @click="goToData" v-if="$router.currentRoute.value.name !== 'home'">Data Dashboard</button>
   </div>
 </template>
 
