a28ef443d67db791d0087a6cbab87612502c46bc
Christina Ernst <13266735+cernst122@users.noreply.github.com>
2023-08-22 13:52:12 -0500

    Attempt to wire individual story views to spreadsheet.

--- frontend/src/views/IndividualStoryView.vue
+++ frontend/src/views/IndividualStoryView.vue
@@ -1,49 +1,22 @@
 <script setup lang="ts">
 import Stories from '@/home/Stories.vue'
 import { useRoute } from 'vue-router';
+import { ref, onMounted } from 'vue';
+import { getData } from '@/components/fetchSpreadsheet'
 
 const route = useRoute();
 
-const stories = [
-  {
-    name: "Dr. Helene D. Gayle, President of Spelman College",
-    quote: "Aspire to inspire before we expire 1.",
-    story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra quam sed purus dignissim accumsan. Phasellus tristique dignissim lacus, ut dapibus libero luctus a. Sed tempor placerat nulla. Vestibulum tincidunt, massa quis vehicula eleifend, quam felis consectetur libero, vitae volutpat arcu felis eget metus. Nullam laoreet hendrerit mauris vitae ultricies. Vivamus hendrerit erat eget felis hendrerit pulvinar. Integer ut lorem consectetur, auctor nibh a, aliquam sem. Nam erat purus, congue vitae tellus ullamcorper, porttitor pellentesque lectus. Proin sed posuere eros. Phasellus at suscipit nunc, vel maximus tellus. Vestibulum lorem nisi, dapibus malesuada metus id, lacinia ultricies magna. Aenean iaculis ante est, ac euismod dolor vehicula non.",
-    imgSrc: "/story_placeholder1.jpg"
-  },
-  {
-    name: "Shakiyla Huggins - Meeting the Challenge",
-    quote: "Aspire to inspire before we expire 2.",
-    story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra quam sed purus dignissim accumsan. Phasellus tristique dignissim lacus, ut dapibus libero luctus a. Sed tempor placerat nulla. Vestibulum tincidunt, massa quis vehicula eleifend, quam felis consectetur libero, vitae volutpat arcu felis eget metus. Nullam laoreet hendrerit mauris vitae ultricies. Vivamus hendrerit erat eget felis hendrerit pulvinar. Integer ut lorem consectetur, auctor nibh a, aliquam sem. Nam erat purus, congue vitae tellus ullamcorper, porttitor pellentesque lectus. Proin sed posuere eros. Phasellus at suscipit nunc, vel maximus tellus. Vestibulum lorem nisi, dapibus malesuada metus id, lacinia ultricies magna. Aenean iaculis ante est, ac euismod dolor vehicula non.",
-    imgSrc: "/story_placeholder2.jpg"
-  },
-  {
-    name: "Deanna Clemmer - An Invaluable Lab",
-    quote: "Aspire to inspire before we expire 3.",
-    story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pharetra quam sed purus dignissim accumsan. Phasellus tristique dignissim lacus, ut dapibus libero luctus a. Sed tempor placerat nulla. Vestibulum tincidunt, massa quis vehicula eleifend, quam felis consectetur libero, vitae volutpat arcu felis eget metus. Nullam laoreet hendrerit mauris vitae ultricies. Vivamus hendrerit erat eget felis hendrerit pulvinar. Integer ut lorem consectetur, auctor nibh a, aliquam sem. Nam erat purus, congue vitae tellus ullamcorper, porttitor pellentesque lectus. Proin sed posuere eros. Phasellus at suscipit nunc, vel maximus tellus. Vestibulum lorem nisi, dapibus malesuada metus id, lacinia ultricies magna. Aenean iaculis ante est, ac euismod dolor vehicula non.",
-    imgSrc: "/story_placeholder3.jpg"
-  },
-  {
-    name: "4",
-    quote: "Aspire to inspire before we expire 4.",
-    story: "Story 4",
-    imgSrc: "/story_placeholder1.jpg"
-  },
-  {
-    name: "5",
-    quote: "Aspire to inspire before we expire 5.",
-    story: "Story 5",
-    imgSrc: "/story_placeholder2.jpg"
-  },
-  {
-    name: "6",
-    quote: "Aspire to inspire before we expire 6.",
-    story: "Story 6",
-    imgSrc: "/story_placeholder3.jpg"
-  },
-]
+const sid = import.meta.env.VITE_WOMEN_CONTENT_SHEET_ID
 
-let thisStory = stories[route.params.id];
+const StoriesURI = `https://docs.google.com/spreadsheets/d/${sid}/export?format=csv`
+const rows = ref([]);
+
+onMounted(() => {
+  getData(StoriesURI, rows);
+});
+
+
+let thisStory = rows[route.params.id];
 </script>
 
 <template>
@@ -51,7 +24,7 @@ let thisStory = stories[route.params.id];
     <router-link id="back-button" class="material-icons" :to="{ name: 'stories' }">chevron_left</router-link>
     <div class="side-by-side">
       <div>
-        <img class="profile-img" :src="thisStory.imgSrc" />
+        <img class="profile-img" :src="thisStory.hosted_image_link" />
         <div class="quote">
           "{{ thisStory.quote }}"
         </div>
@@ -62,7 +35,7 @@ let thisStory = stories[route.params.id];
       </div>
       <div>
         <h3>{{ thisStory.name }}</h3>
-        <p>{{ thisStory.story }}</p>
+        <p>{{ thisStory.text }}</p>
       </div>
     </div>
   </div>
