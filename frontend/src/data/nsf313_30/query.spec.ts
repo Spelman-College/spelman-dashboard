9cd01653161d73e16b6af7e95389653194cbda23
Amier Smith <amiers@google.com>
2023-08-22 15:47:32 -0400

    describe tests

--- frontend/src/data/nsf313_30/query.spec.ts
+++ frontend/src/data/nsf313_30/query.spec.ts
@@ -18,30 +18,14 @@ const all_enrollment_types = Categories['collegeOrGraduateSchoolEnrollment']
 
 const all_university_levels = Categories['collegeOrUniversityLevel']
 
-describe('demo query', () => {
+describe('test omitted summary statistic', () => {
 
-    test('test race alone', () => {
+    test('test race catgeory dependency', () => {
         let out = demo.query(black)
         expect(out.error).toEqual(undefined)
         expect(out.results).toEqual([
             "Count_Person_EnrolledInCollegeOrGraduateSchool_Female_HBCU_Black",
             "Count_Person_EnrolledInCollegeOrGraduateSchool_Male_HBCU_Black",
-        ])
-    })
-
-    test('test race enrollment', () => {
-        let out = demo.query(black, private_enrollment)
-        expect(out.error).toEqual(undefined)
-        expect(out.results).toEqual([
-            "Count_Person_EnrolledInPrivateCollegeOrGraduateSchool_Female_HBCU_Black",
-            "Count_Person_EnrolledInPrivateCollegeOrGraduateSchool_Male_HBCU_Black",])
-    })
-
-    test('test gender alone', () => {
-        let out = demo.query(female)
-        expect(out.error).toEqual(undefined)
-        expect(out.results).toEqual([
-            "Count_Person_EnrolledInCollegeOrGraduateSchool_Female_HBCU"
         ])
     })
 
@@ -53,13 +37,16 @@ describe('demo query', () => {
         ])
     })
 
-    test('test race gender enrollment', () => {
-        let out = demo.query(female, private_enrollment, black)
+    test('test gender alone', () => {
+        let out = demo.query(female)
         expect(out.error).toEqual(undefined)
-        expect(out.results).toEqual(["Count_Person_EnrolledInPrivateCollegeOrGraduateSchool_Female_HBCU_Black"])
+        expect(out.results).toEqual([
+            "Count_Person_EnrolledInCollegeOrGraduateSchool_Female_HBCU"
+        ])
     })
+})
 
-
+describe('test each single dimension', () => {
     test('test each enrollement', () => {
         all_enrollment_types.forEach((type) => {
             let query = new Query("collegeOrGraduateSchoolEnrollment", type)
@@ -85,6 +72,23 @@ describe('demo query', () => {
             let out = demo.query(query_a, query_b, query_c)
             expect(out.results).toEqual(["Count_Person_EnrolledInPublicCollegeOrGraduateSchool_".concat(level).concat("_Female_HBCU")])
         })
+    })
+})
+
+describe('test combination queries', () => {
+
+    test('test race enrollment', () => {
+        let out = demo.query(black, private_enrollment)
+        expect(out.error).toEqual(undefined)
+        expect(out.results).toEqual([
+            "Count_Person_EnrolledInPrivateCollegeOrGraduateSchool_Female_HBCU_Black",
+            "Count_Person_EnrolledInPrivateCollegeOrGraduateSchool_Male_HBCU_Black",])
+    })
+
+    test('test race gender enrollment', () => {
+        let out = demo.query(female, private_enrollment, black)
+        expect(out.error).toEqual(undefined)
+        expect(out.results).toEqual(["Count_Person_EnrolledInPrivateCollegeOrGraduateSchool_Female_HBCU_Black"])
     })
 
     test('test 1 enrollment types 1 university level', () => {
