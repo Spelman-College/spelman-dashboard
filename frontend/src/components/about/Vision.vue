12456384b60c7fa1668b5a5fd57cadda2c1fdfcd
Christina Ernst <13266735+cernst122@users.noreply.github.com>
2023-08-23 13:57:29 -0500

    Add photo grid to vision

--- frontend/src/components/about/Vision.vue
+++ frontend/src/components/about/Vision.vue
@@ -5,8 +5,13 @@
     <div id="splash-text">
       <div id="splash-text-header">Our Vision</div>
       <div id="splash-text-body">An international [platform] [showcasing] Black Women in STEM</div>
-      <div id="swiggle"><img src="/swiggly_line_ACF0FF.svg" /></div>  
+      <div id="swiggle-vision-card"><img src="/swiggly_line_ACF0FF.svg" /></div>  
       </div>
+  </div>
+  <div id="building-vision-header">Building the Vision</div>
+  <div id="swiggle"><img src="/swiggly_line_ACF0FF.svg" /></div>
+  <div id="image-grid">
+    <img id="square-photo-card" src="/spelman_flickr_01.jpg"/> 
   </div>
 </template>
 
@@ -30,6 +35,12 @@ div#splash-text {
   margin-left: 0.55rem;
 
   padding: 4rem;
+}
+
+/* TODO */
+div#image-grid{
+  margin-top:3rem;
+  margin-bottom: 26rem;
 }
 
 #splash-text-body {
@@ -71,8 +82,28 @@ img#stripes {
   margin-left: 75.26rem;
 }
 
-#swiggle {
+#swiggle-vision-card {
   margin-left: 7.23rem;
+}
+
+#building-vision-header {
+  color: #FFF;
+  text-align: center;
+  font-family: Google Sans;
+  font-size: 1.875rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 3.75rem; /* 200% */
+  margin-top: 17.31rem;
+}
+
+#square-photo-card {
+  width: 26.375rem;
+  height: 26.375rem;
+  flex-shrink: 0;
+  border-radius: 2.5rem;
+  background: url(<path-to-image>), lightgray 50% / cover no-repeat;
+  object-fit: cover;
 }
 
 </style>
