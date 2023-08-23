f7d414a7513ec91a4015e6e0d27288ac449f3305
Christina Ernst <13266735+cernst122@users.noreply.github.com>
2023-08-23 14:16:23 -0500

    Update client.ts

--- frontend/src/sheets/client.ts
+++ frontend/src/sheets/client.ts
@@ -1,14 +1,15 @@
 import Papa from 'papaparse';
 
 export const getData = async (sheet:string)  => {
-    const out = await Papa.parse(sheet, {
-      header: true,
-      download: true,
-      worker: true,
-      complete (results) {
-        console.log(results.data);
-        Promise.resolve(results.data);
-      },
-    });
-    return out;
+  return new Promise((resolve, reject) => {
+      Papa.parse(sheet, {
+          header: true,
+          download: true,
+          worker: true,
+          complete (results) {
+              console.log(results.data);
+              resolve(results.data)
+          },
+      })
+  })
 }
