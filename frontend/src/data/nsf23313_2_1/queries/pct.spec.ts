6981a7d8c82b94167c764eb4bf83f6b8ddb26037
Amier Smith <amiers@google.com>
2023-08-25 13:54:57 -0500

    tests for new dcids

--- frontend/src/data/nsf23313_2_1/queries/pct.spec.ts
+++ frontend/src/data/nsf23313_2_1/queries/pct.spec.ts
@@ -7,9 +7,8 @@ const data: Query_nsf23313_2_1_pct = new Query_nsf23313_2_1_pct()
 const male = new Query('gender', 'Male')
 
 
-const hispanic = new Query('ethnicity', 'HispanicOrLatino')
-const masters_degree = new Query('education', 'EducationalAttainmentMastersDegree')
-const doc_degree = new Query('education', 'EducationalAttainmentDoctorateDegree')
+const masters_degree = new Query('education', 'MastersDegree')
+const doc_degree = new Query('education', 'DoctorateDegree')
 const postdoc = new Query('occupation', 'PostDoctoralAppointeeOccupation')
 const nonfaculty = new Query('occupation', 'NonFacultyResearcherOccupation')
 const black = new Query('race', 'Black')
@@ -22,21 +21,14 @@ describe('nsf23313_2_1 query single dimenions that are not reported alone', () =
     test('query single category gender', () => {
         const out = data.query(male)
         expect(out.results).toEqual([
-            "Count_Person_EducationalAttainmentGraduateSchoolOrPostGraduate_Male_AsAFractionOf_Count_Person_EducationalAttainmentGraduateSchoolOrPostGraduate"
-        ])
-    })
-
-    test('query single category ethnicity', () => {
-        const out = data.query(hispanic)
-        expect(out.results).toEqual([
-            "Count_Person_Citizen_EducationalAttainmentGraduateSchoolOrPostGraduate_HispanicOrLatino_AsAFractionOf_Count_Person_EducationalAttainmentGraduateSchoolOrPostGraduate"
+            "Percent_Male_In_Count_Student_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree"
         ])
     })
 
     test('query single category race', () => {
         const out = data.query(black)
         expect(out.results).toEqual([
-            "Count_Person_Citizen_EducationalAttainmentGraduateSchoolOrPostGraduate_NotHispanicOrLatino_Black_AsAFractionOf_Count_Person_EducationalAttainmentGraduateSchoolOrPostGraduate"
+            "Percent_Citizen_NotHispanicOrLatino_Black_In_Count_Student_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree"
         ])
     })
 
@@ -49,52 +41,45 @@ describe('nsf23313_2_1 query single dimension that is expected to return a value
     test('query single category masters degree', () => {
         const out = data.query(masters_degree)
         expect(out.results).toEqual([
-            'Count_Person_EducationalAttainmentMastersDegree_AsAFractionOf_Count_Person_EducationalAttainmentMastersDegree',
         ])
     })
 
     test('query citizens', () => {
         const out = data.query(citizen)
         expect(out.results).toEqual([
-            'Count_Person_Citizen_EducationalAttainmentGraduateSchoolOrPostGraduate_AsAFractionOf_Count_Person_EducationalAttainmentGraduateSchoolOrPostGraduate',
+            'Percent_Citizen_In_Count_Student_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree',
         ])
     })
 
     test('query visa holders', () => {
         const out = data.query(visa)
         expect(out.results).toEqual([
-            'Count_Person_VisaHolder_EducationalAttainmentGraduateSchoolOrPostGraduate_UnknownEthnicity_AsAFractionOf_Count_Person_EducationalAttainmentGraduateSchoolOrPostGraduate',
+            'Percent_UnknownEthnicity_VisaHolder_In_Count_Student_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree',
         ])
     })
 
     test('query unknown race', () => {
         const out = data.query(unknown)
         expect(out.results).toEqual([
-            'Count_Person_Citizen_EducationalAttainmentGraduateSchoolOrPostGraduate_UnknownEthnicity_UnknownRace_AsAFractionOf_Count_Person_EducationalAttainmentGraduateSchoolOrPostGraduate',
+            'Percent_Citizen_UnknownEthnicity_UnknownRace_In_Count_Student_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree',
         ])
     })
 })
 
 describe('nsf23313_2_1 query 2 categories', () => {
-    test('query masters degree hispanic', () => {
-        const out = data.query(masters_degree, hispanic)
-        expect(out.results).toEqual([
-            'Count_Person_Citizen_EducationalAttainmentMastersDegree_HispanicOrLatino_AsAFractionOf_Count_Person_EducationalAttainmentMastersDegree',
-        ])
-    })
 
     test('query postdoc appointee citizens', () => {
         const out = data.query(citizen,postdoc)
-        expect(out.results).toEqual([])
+        expect(out.results).toEqual(["Percent_Citizen_In_Count_Person_ScienceOrEngineeringOrHealth_EducationalAttainmentDoctorateDegree_PostDoctoralAppointeeOccupation"])
     })
 
     test('query citizens with masters ', () => {
         const out = data.query(citizen, masters_degree)
-        expect(out.results).toEqual(["Count_Person_Citizen_EducationalAttainmentMastersDegree_AsAFractionOf_Count_Person_EducationalAttainmentMastersDegree"])
+        expect(out.results).toEqual(["Percent_Citizen_In_Count_Student_ScienceOrEngineeringOrHealth_MastersDegree"])
     })
 
     test('query visa holding doctorate degrees', () => {
         const out = data.query(visa, doc_degree)
-        expect(out.results).toEqual(["Count_Person_VisaHolder_EducationalAttainmentDoctorateDegree_UnknownEthnicity_AsAFractionOf_Count_Person_EducationalAttainmentDoctorateDegree"])
+        expect(out.results).toEqual(["Percent_UnknownEthnicity_VisaHolder_In_Count_Student_ScienceOrEngineeringOrHealth_DoctorateDegree"])
     })
 })
--- frontend/src/data/nsf23313_2_1/queries/values.spec.ts
+++ frontend/src/data/nsf23313_2_1/queries/values.spec.ts
@@ -5,9 +5,8 @@ import { Query } from '../../queries/query'
 const data: Query_nsf23313_2_1_values = new Query_nsf23313_2_1_values()
 
 const male = new Query('gender', 'Male')
-const hispanic = new Query('ethnicity', 'HispanicOrLatino')
-const masters_degree = new Query('education', 'EducationalAttainmentMastersDegree')
-const doc_degree = new Query('education', 'EducationalAttainmentDoctorateDegree')
+const masters_degree = new Query('education', 'MastersDegree')
+const doc_degree = new Query('education', 'DoctorateDegree')
 const postdoc = new Query('occupation', 'PostDoctoralAppointeeOccupation')
 const nonfaculty = new Query('occupation', 'NonFacultyResearcherOccupation')
 const black = new Query('race', 'Black')
@@ -19,79 +18,68 @@ describe('nsf23313_2_1 query single dimenions that are not reported alone', () =
     test('query single category gender', () => {
         const out = data.query(male)
         expect(out.results).toEqual([
-            "Count_Person_EducationalAttainmentGraduateSchoolOrPostGraduate_Male"
-        ])
-    })
-
-    test('query single category ethnicity', () => {
-        const out = data.query(hispanic)
-        expect(out.results).toEqual([
-            "Count_Person_Citizen_EducationalAttainmentGraduateSchoolOrPostGraduate_HispanicOrLatino"
+            "Count_Student_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree_Male"
         ])
     })
 
     test('query single category race', () => {
         const out = data.query(black)
         expect(out.results).toEqual([
-            "Count_Person_Citizen_EducationalAttainmentGraduateSchoolOrPostGraduate_NotHispanicOrLatino_Black"
+            "Count_Student_Citizen_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree_NotHispanicOrLatino_Black"
         ])
     })
 
     test('query nonfaculty', () => {
         const out = data.query(nonfaculty)
-        expect(out.results).toEqual([])
+        expect(out.results).toEqual(['Count_Person_EducationalAttainmentDoctorateDegree_ScienceOrEngineeringOrHealth_NonFacultyResearcherOccupation'])
     })
 })
 describe('nsf23313_2_1 query single dimension that is expected to return a value', () => {
     test('query single category masters degree', () => {
         const out = data.query(masters_degree)
         expect(out.results).toEqual([
-            'Count_Person_EducationalAttainmentMastersDegree',
+            'Count_Student_ScienceOrEngineeringOrHealth_MastersDegree',
         ])
     })
 
     test('query citizens', () => {
         const out = data.query(citizen)
         expect(out.results).toEqual([
-            'Count_Person_Citizen_EducationalAttainmentGraduateSchoolOrPostGraduate',
+            'Count_Student_Citizen_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree',
         ])
     })
 
     test('query visa holders', () => {
         const out = data.query(visa)
         expect(out.results).toEqual([
-            'Count_Person_VisaHolder_EducationalAttainmentGraduateSchoolOrPostGraduate_UnknownEthnicity',
+            'Count_Student_VisaHolder_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree_UnknownEthnicity',
         ])
     })
 
     test('query unknown race', () => {
         const out = data.query(unknown)
         expect(out.results).toEqual([
-            'Count_Person_Citizen_EducationalAttainmentGraduateSchoolOrPostGraduate_UnknownEthnicity_UnknownRace',
+            'Count_Student_Citizen_ScienceOrEngineeringOrHealth_MastersDegreeOrDoctorateDegree_UnknownEthnicity_UnknownRace',
         ])
     })
 })
 
 describe('nsf23313_2_1 query 2 categories', () => {
-    test('query masters degree hispanic', () => {
-        const out = data.query(masters_degree, hispanic)
-        expect(out.results).toEqual([
-            'Count_Person_Citizen_EducationalAttainmentMastersDegree_HispanicOrLatino',
-        ])
-    })
 
     test('query postdoc appointee citizens', () => {
         const out = data.query(citizen,postdoc)
-        expect(out.results).toEqual([])
+        expect(out.results).toEqual([
+            "Count_Person_Citizen_EducationalAttainmentDoctorateDegree_ScienceOrEngineeringOrHealth_PostDoctoralAppointeeOccupation"
+        ])
     })
 
     test('query citizens with masters ', () => {
         const out = data.query(citizen, masters_degree)
-        expect(out.results).toEqual(["Count_Person_Citizen_EducationalAttainmentMastersDegree"])
+        expect(out.results).toEqual(["Count_Student_Citizen_ScienceOrEngineeringOrHealth_MastersDegree"])
     })
 
     test('query visa holding doctorate degrees', () => {
         const out = data.query(visa, doc_degree)
-        expect(out.results).toEqual(["Count_Person_VisaHolder_EducationalAttainmentDoctorateDegree_UnknownEthnicity"])
+        expect(out.results).toEqual(["Count_Student_VisaHolder_ScienceOrEngineeringOrHealth_DoctorateDegree_UnknownEthnicity"])
     })
 })
