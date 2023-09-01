3b14ef1d1da2e981b0a4f1e176dc8c13b3e1a5ac
xewar <92114989+xewar@users.noreply.github.com>
2023-09-01 16:01:25 -0400

    Helping Homepage's splash be more responsive on desktop window sizes

--- frontend/src/home/Splash.vue
+++ frontend/src/home/Splash.vue
@@ -9,38 +9,48 @@ function goToData() {
 </script>
 
 <template>
-  <h1>
-    Breaking Boundaries<br />
-    and Transforming<br />
-    Futures
-  </h1>
-  <p>
-    Amplifying the stories and impact of Black women in STEM<br />
-    through data, insights, and advocacy
-  </p>
-  <button @click="goToData">Visit the Data Dashboard</button>
-  <div id="splash-graphics">
-    <img src="/splash.png" alt="" id="splash-image" />
-    <img src="/swiggly_red_FF6454.svg" alt="" id="swiggly-red" />
-    <div id="plusses">
-      <img src="/plus_icon_1.svg" alt="" class="plus-icon" id="plus-icon-1" />
-      <img src="/plus_icon_2.svg" alt="" class="plus-icon" id="plus-icon-2" />
-      <img src="/plus_icon_3.svg" alt="" class="plus-icon" id="plus-icon-3" />
+  <div class="flex-container">
+    <div class="title-and-link">
+      <h1>
+        Breaking Boundaries<br />
+        and Transforming<br />
+        Futures
+      </h1>
+      <p>
+        Amplifying the stories and impact of Black women in STEM<br />
+        through data, insights, and advocacy
+      </p>
+      <button @click="goToData">Visit the Data Dashboard</button>
     </div>
-  </div>
+      <div id="splash-graphics">
+        <img src="/splash.png" alt="" id="splash-image" />
+        <img src="/swiggly_red_FF6454.svg" alt="" id="swiggly-red" />
+        <div id="plusses">
+          <img src="/plus_icon_1.svg" alt="" class="plus-icon" id="plus-icon-1" />
+          <img src="/plus_icon_2.svg" alt="" class="plus-icon" id="plus-icon-2" />
+          <img src="/plus_icon_3.svg" alt="" class="plus-icon" id="plus-icon-3" />
+        </div>
+      </div>
+    </div>
 </template>
 
 <style scoped>
 @import "/homepage.css";
 
+.flex-container{
+  display:flex;
+  justify-content:space-between;
+}
+
 #splash-graphics {
-  position: absolute;
-  top: 20rem;
-  left: 70%;
+  position: relative;
+  top: 10rem;
+  /* Temporary fix to prevent swiggly red from going over title text
+   - update margin-left once mobile view is designed */
+  margin-left:6rem; 
 }
 
 #splash-image {
-  position: absolute;
   width: 24rem;
 }
 
