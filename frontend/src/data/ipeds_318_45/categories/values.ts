a63b7ef5c9609dae36b2d25a3106a3f1be21f5b1
isaac-looker <drumsound@google.com>
2023-09-06 19:18:24 +0000

    add more constants

--- frontend/src/data/ipeds_318_45/categories/values.ts
+++ frontend/src/data/ipeds_318_45/categories/values.ts
@@ -1,26 +1,31 @@
+import * as dims from '../../queries/dimensions'
+
 export const Categories = {
-  gender: new Set<string>(['Male', 'Female']),
-  ethnicity: new Set<string>([
-    'HispanicOrLatino',
-    'AmericanIndianOrAlaskaNativeAlone',
-    'Black',
-    'TwoOrMoreRaces',
-    'WhiteAlone',
+  'gender': new Set<string>([
+    dims.Male,
+    dims.Female,
+  ]),
+  'ethnicity': new Set<string>([
+    dims.HispanicOrLatino,
+    dims.AmericanIndianOrAlaskaNativeAlone,
+    dims.Black,
+    dims.TwoOrMoreRaces,
+    dims.WhiteAlone,
 
     // AsianOrPacificIslander is the sum of both Asian AND HawaiianNativeOrPacificIslander
     // 'AsianOrPacificIslander',
-    'Asian',
-    'HawaiianNativeOrPacificIslander',
+    dims.Asian,
+    dims.HawaiianNativeOrPacificIslander,
 
     // Total for this category is reported, including this data.
-    'NonUSResident'
+    dims.NonUSResident
   ]),
-  education: new Set<string>([
-    'EducationalAttainment9ThTo12ThGradeNoDiploma',
-    'EducationalAttainmentAssociatesDegree',
-    'EducationalAttainmentBachelorsDegree',
-    'EducationalAttainmentCollegeGraduate',
-    'EducationalAttainmentDoctorateDegree',
-    'EducationalAttainmentMastersDegree'
-  ])
+  'education': new Set<string>([
+    dims.EducationalAttainment9ThTo12ThGradeNoDiploma,
+    dims.EducationalAttainmentAssociatesDegree,
+    dims.EducationalAttainmentBachelorsDegree,
+    dims.EducationalAttainmentCollegeGraduate,
+    dims.EducationalAttainmentDoctorateDegree,
+    dims.EducationalAttainmentMastersDegree,
+  ]),
 }
