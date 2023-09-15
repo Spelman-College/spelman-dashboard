ccd24311b354fbe0fefedd6f7c02f896b693b177
xewar <xewar@users.noreply.github.com>
2023-09-15 00:31:48 +0000

    Apply formatting changes

--- frontend/src/components/dashboard/DatasetSelector.vue
+++ frontend/src/components/dashboard/DatasetSelector.vue
@@ -148,8 +148,8 @@ function handleClick(name: String | null) {
   background-color: #ffffff;
   border-radius: 0.25rem;
   box-shadow: 0px 2px 6px 2px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.3);
-  height:265px;
-  overflow:auto;
+  height: 265px;
+  overflow: auto;
 }
 
 .dropdown-option {
